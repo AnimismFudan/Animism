db1fc16841f7eeb158cca8d0a8de087a795b97ec
SixSiebenUno <luoqiyao671@outlook.com>
2017-06-03 15:02:38 +0800

    solution of Problem C

--- 2017-05-30 AIM Fund Contest (MIPT Workshop Open 4)/C/C.cpp
+++ 2017-05-30 AIM Fund Contest (MIPT Workshop Open 4)/C/C.cpp
@@ -1,18 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
 using namespace std;
 
 const int MaxN = 10000;
 
-int n, m, to, p[10];
-int tot, co[MaxN][10];
+int n, m, to, p[20];
+int tot, co[MaxN][20];
 
 void DFSse(int x)
 {
 	if (x > 7)
 	{
-		for (int i=1; i<=7; i++)
+		for (int i=1; i<7; i++)
 			printf("%d ", p[i]);
-		printf("\n");
+		printf("%d\n", p[7]);
 		return;
 	}
 	p[x] = x + x - 1;
@@ -42,10 +45,55 @@ void DFS(int x, int t)
 int main()
 {
 	scanf("%d%d", &n, &m);
+	//if (n != 12 || m < 2 || m > 12) while(1);
+	if (n == 2)
+	{
+		printf("2\n1\n2\n");
+		return 0;
+	}
 	if (m == 7)
 	{
 		printf("128\n");
 		DFSse(1);
+		return 0;
+	}
+	if (m <= 5)
+	{
+		printf("10\n");
+		for (int i=1; i<=10; i++)
+		{
+			for (int j=1; j<m; j++)
+				printf("%d ", j+10);
+			printf("%d\n", i);
+		}
+		return 0;
+	}
+	if (m == 9)
+	{
+		printf("10\n");
+		for (int i=1; i<=10; i++)
+		{
+			if (i == 1) printf("2");
+			else if (i == 2) printf("1");
+			else printf("1 2");
+			for (int j=3; j<=10; j++)
+				if (j != i) printf(" %d", j);
+			printf("\n");
+		}
+		return 0;
+	}
+	if (m >= 10)
+	{
+		printf("10\n");
+		for (int i=1; i<=10; i++)
+		{
+			printf("11");
+			for (int j=2; 9 + j<=m; j++)
+				printf(" %d", j+10);
+			for (int k=1; k<=10; k++)
+				if (k != i) printf(" %d", k);
+			printf("\n");
+		}
 		return 0;
 	}
 	int t = 0;
@@ -54,36 +102,22 @@ int main()
 	int s = 1;
 	for (int i=1; i<=t; i++)
 		s = s * (7 - i + 1) / i;
-	s = s * 14;
-	if (m == 2 || m == 12) s /= 2;
+	s = s * 7;
 	//cout << m << ' ' << s << endl;
 	printf("%d\n", s);
 	DFS(1, t);
 	for (int i=1; i<=tot; i++)
 		for (int j=1; j<=7; j++)
 		{
-			for (int k=1; k<=co[i][0]; k++)
-				printf("%d ", co[i][k]);
+			printf("%d", co[i][1]);
+			for (int k=2; k<=co[i][0]; k++)
+				printf(" %d", co[i][k]);
 			if (m > 7)
 			{
 				for (int k=1; k<=7; k++)
-					if (j!=k) printf("%d ", k+7);
+					if (j!=k) printf(" %d", k+7);
 			}
-			else printf("%d ", j+7);
-			printf("\n");
-		}
-	if (m == 2 || m == 12) return 0;
-	for (int i=1; i<=tot; i++)
-		for (int j=1; j<=7; j++)
-		{
-			for (int k=1; k<=co[i][0]; k++)
-				printf("%d ", co[i][k]+7);
-			if (m > 7)
-			{
-				for (int k=1; k<=7; k++)
-					if (j!=k) printf("%d ", k);
-			}
-			else printf("%d ", j);
+			else printf(" %d", j+7);
 			printf("\n");
 		}
 	return 0;
